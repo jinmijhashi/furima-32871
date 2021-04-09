ee2115a7974f734a58424211c19127f3125b8bf7
jinmijhashi <77250985+jinmijhashi@users.noreply.github.com>
2021-04-09 19:06:16 +0900

    ユーザー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+    binding.pry
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 
 
-  private
-
+ 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '1234'
     end
   end
 end
+  
+
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,9 @@
 class ItemsController < ApplicationController
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '1234'
+    end
+  end
+
   
 end
