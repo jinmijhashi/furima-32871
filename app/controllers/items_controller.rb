17d0746ac8b03c6832112f77c2df3a390c49321c
jinmijhashi <77250985+jinmijhashi@users.noreply.github.com>
2021-04-13 06:01:54 +0900

    ログイン、ログアウト実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,6 @@
 class ItemsController < ApplicationController
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '1234'
-    end
+   before_action :authenticate_user!, except: [:index]
+  def index
   end
 
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :bitrhday, presence: true
+  validates :birth_date, presence: true
 end 
