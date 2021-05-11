4d802b3bb8695fca33a7d20082a084c95cbb904f
jinmijhashi <77250985+jinmijhashi@users.noreply.github.com>
2021-05-11 14:22:36 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
     
 
      const addProfitDom = document.getElementById("profit");
-      const value_result = inputValue * 0.1
+      const value_result = (Math.floor(inputValue * 0.1));
       
       addProfitDom.innerHTML = (Math.floor(inputValue - value_result));
     
