a89ad05a50c7000a05ba6c81a43028e11305a4f2
jinmijhashi <77250985+jinmijhashi@users.noreply.github.com>
2021-05-21 20:49:54 +0900

    エラーハンドリングまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @items = Item.includes(:user).order('created_at DESC')
+    
   end
 
   def new
@@ -21,10 +23,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit 
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit  
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:title, :text, :image, :category_id, :status_id, :shipping_id, :shipping_area_id,
                                  :shipdate_id, :price).merge(user_id: current_user.id)
   end
+
+ 
 end
